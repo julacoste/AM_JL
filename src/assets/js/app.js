4776365dfdc405a1f46fbbe8aa37c47120800ee2
julacoste <ju_lacoste@yahoo.fr>
2020-12-02 14:18:10 +0100

    Fix overlay, adjust animation for band menu and init menu band lvl 2

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,23 @@
 'use strict';
 
+//variables declaration
 var $band = $('.am_band');
+var $overlay = $('.am_overlay');
+
+// click on rayons = opening tabs + active overlay
 $('.rayons').on('click', function(e) {
     e.preventDefault();
 
     $band.addClass('active');
+    $overlay.addClass('active');
+
 
 })
 
+// click on close = closing tabs + unactive overlay
 $('.am_band__close').on('click', function(e) {
     e.preventDefault();
 
     $band.removeClass('active');
+    $overlay.removeClass('active');
 })
