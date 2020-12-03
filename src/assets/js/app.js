f4993e79e9e9c4e3cbb16bb4d6ec5a4f8a5ebe81
julacoste <ju_lacoste@yahoo.fr>
2020-12-03 14:23:58 +0100

    Create animation for menu from lvl 1 to lvl 2 and update semantic html, update scss for menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,9 +15,24 @@ $('.rayons').on('click', function(e) {
 })
 
 // click on close = closing tabs + unactive overlay
-$('.am_band__close').on('click', function(e) {
+$('.am_band__close, .am_overlay').on('click', function(e) {
     e.preventDefault();
 
     $band.removeClass('active');
     $overlay.removeClass('active');
+})
+
+// click on epicerie salee = hide nav lvl1 + show nav lvl 2
+$('.am_band__nav--level1 .am_band__item .am_btn').on('click', function(e) {
+    e.preventDefault();
+    $('.am_band__nav--level2').addClass('active');
+    $('.am_band__nav--level1').addClass('submenu-active');
+});
+
+
+//click on btn dropdown = toggle display
+$('.am_btn--dropdown').on('click', function(e) {
+    e.preventDefault();
+
+    $('.am_dropdown__content').dropdown();
 })
